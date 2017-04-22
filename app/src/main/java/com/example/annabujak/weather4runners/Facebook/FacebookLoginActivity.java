70ced925c13428f273ab52b2303fbb6d4fbeaa8e
posejdon <aniutka.pop@gmail.com>
2017-04-23 00:12:26 +0200

    Działające logowanie przez facebooka i dodawanie do bazy nowego usera

---
+++ app/src/main/java/com/example/annabujak/weather4runners/Facebook/FacebookLoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.annabujak.weather4runners.Facebook;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.annabujak.weather4runners.Database.DBWeather4Runners;
+import com.example.annabujak.weather4runners.Database.User;
+import com.example.annabujak.weather4runners.MainActivity;
+import com.example.annabujak.weather4runners.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class FacebookLoginActivity extends AppCompatActivity {
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    DBWeather4Runners database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.facebook_activity);
+        database = new DBWeather4Runners(this);
+
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        if(profilExists())
+            startMainActivity();
+        registerLogin();
+    }
+    private boolean profilExists(){
+        Profile profile = Profile.getCurrentProfile();
+        if (profile != null)
+            return true;
+        return false;
+    }
+    private void startMainActivity(){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+    private void addUserData(){
+        Profile profile = Profile.getCurrentProfile();
+        database.addUser(new User(profile.getFirstName(),profile.getLastName()));
+        database.close();
+    }
+    private void registerLogin(){
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                database.clearUser();
+                addUserData();
+                startMainActivity();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
